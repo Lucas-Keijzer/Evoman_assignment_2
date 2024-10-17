eb46dd93795849bcaac14e45612b0bb9031ae801
PjotrPiet <Pjotrpiet@outlook.com>
2024-10-17 21:51:02 +0200

    missen twee voor ea2

--- get_best_individuals.py
+++ get_best_individuals.py
@@ -42,7 +42,10 @@ def main():
 
     # all possible triples of enemies
 
-    enemy_groups = list(itertools.combinations(range(1, 9), 3))[::-1][:15]
+    enemy_groups = list(itertools.combinations(range(1, 9), 3))[41:46]
+    # i_s = enemy_groups.index((3,5,8))
+    # i_e = enemy_groups.index((3,6,8))
+    # print(i_s, i_e)
     # print(len(enemy_groups))
     # enemy_groups = [[1, 2, 3]]  # use only the first group for testing comment dit ff uit wanneer je alles runt
 
